82d393e7779b5812ed97ad9ffc032c238cc7f6f2
HoWilgh <wilfried.hortschitz@donau-uni.ac.at>
2025-03-05 11:48:29 +0100

    Add script to check folder structure for simulation datasets

---
+++ data_preview/check_file_and_folder_structure.py
@@ -0,0 +1,93 @@
+import os
+import argparse
+
+
+def check_readme_exists(folder_path):
+    readme_path = os.path.join(folder_path, 'README')
+    if os.path.isfile(readme_path):
+        print(f"File 'README' exists in the folder '{folder_path}'.")
+        return True
+    else:
+        print(f"File 'README' does not exist in the folder '{folder_path}'.")
+        return False
+
+
+def check_subfolders_exist(folder_path):
+    subfolders = ['GS', 'Jij', 'MC']
+
+    for subfolder in subfolders:
+        subfolder_path = os.path.join(folder_path, subfolder)
+        if os.path.isdir(subfolder_path):
+            print(
+                f"Subfolder '{subfolder}' exists in the folder '{folder_path}'.")
+            all_exist = True
+        else:
+            print(
+                f"Subfolder '{subfolder}' does not exist in the folder '{folder_path}'.")
+            all_exist = False
+    return all_exist
+
+
+def check_structure_cif_file_exists(folder_path):
+    structure_file_path = os.path.join(folder_path, 'structure.cif')
+    if os.path.isfile(structure_file_path):
+        print(
+            f"File 'structure.cif' exists in the folder '{folder_path}'.")
+        return True
+    else:
+        print(
+            f"File 'structure.cif' does not exist in the folder '{folder_path}'.")
+        return False
+
+
+def check_structure(folder_path):
+    if not os.path.isdir(folder_path):
+        print(
+            f"The main folder '{folder_path}' for the dataset does not exist.")
+        main_folder_exists = False
+    else:
+        print(f"The main folder '{folder_path}' for the dataset exists.")
+        main_folder_exists = True
+
+        readme_exists = check_readme_exists(folder_path)
+        subfolders_exist = check_subfolders_exist(folder_path)
+        structure_cif_exists = check_structure_cif_file_exists(folder_path)
+
+        if readme_exists and subfolders_exist and structure_cif_exists:
+            print("# All checks passed.")
+            return True
+        else:
+            print("# Some checks failed.")
+            return False
+
+# TODO: Add a check if the main folder exists
+# Optional-TODO: Add a check if dataset is .zip file and extract it
+
+
+def main(args):
+
+    dataset_name = 'Co2Fe2H4'  # Default value
+
+    if args.dataset_name:
+        # print("The dataset is given: " +
+        #       args.dataset_name)
+        dataset_name = args.dataset_name
+
+    check_structure(args.dataset_name)
+
+
+if __name__ == "__main__":
+    # Initialize parser
+    parser = argparse.ArgumentParser(
+        description="Check folder structure of a simulation data set for the 'Electronic' and 'Atomistic' model.")
+
+    # Adding optional argument
+    parser.add_argument('-dataset_name', type=str, default='Co2Fe2H4',
+                        help='Name of the folder or dataset to check (default: Co2Fe2H4)')
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    print(args)
+
+    main(args)
